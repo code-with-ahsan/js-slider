5239df587eecf3abcf31ef480ab4af606cbe7900
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-09-17 13:43:40 +0200

    javascript and css slider using multiple images on DOM

--- script.js
+++ script.js
@@ -3,38 +3,71 @@ const images = [
   "images/image2.png",
   "images/image3.png",
   "images/image4.png",
-]
+];
+
+const DIRECTIONS = {
+  LEFT: "left",
+  RIGHT: "right",
+};
 
 let currentImage = null;
-const activeImgEl = document.getElementById('activeImage');
-const leftArrowEl = document.getElementById('leftArrow');
-const rightArrowEl = document.getElementById('rightArrow');
+const leftArrowEl = document.getElementById("leftArrow");
+const rightArrowEl = document.getElementById("rightArrow");
 
+const createImages = () => {
+  const containerEl = document.querySelector(".images-container");
+  images.forEach((imageSrc) => {
+    const imgEl = document.createElement("img");
+    imgEl.setAttribute("src", imageSrc);
+    containerEl.append(imgEl);
+  });
+};
 
-const setActiveImage = (targetImage) => {
+createImages();
+
+const setActiveImage = (targetImage, direction) => {
   currentImage = targetImage;
-  console.log("🚀 ~ file: script.js ~ line 16 ~ setActiveImage ~ currentImage", currentImage)
-  activeImgEl.setAttribute('src', targetImage);
-}
+  const activeImgEl = document.querySelector(".images-container img.active");
+  if (activeImgEl) {
+    activeImgEl.classList.forEach((cssClass) => {
+      if (cssClass.startsWith("animation-")) {
+        activeImgEl.classList.remove(cssClass);
+      }
+    });
+    if (direction === DIRECTIONS.LEFT) {
+      activeImgEl.classList.add("animation-slide-to-left");
+    } else if (direction === DIRECTIONS.RIGHT) {
+      activeImgEl.classList.add("animation-slide-to-right");
+    }
+    activeImgEl.classList.remove("active");
+  }
+  const nextActiveEl = document.querySelector(`img[src="${currentImage}"]`);
+  nextActiveEl.classList.add("active");
+  if (direction === DIRECTIONS.LEFT) {
+    nextActiveEl.classList.add("animation-slide-from-right");
+  } else if (direction === DIRECTIONS.RIGHT) {
+    nextActiveEl.classList.add("animation-slide-from-left");
+  }
+};
 
-setActiveImage(images[3])
+setActiveImage(images[0], DIRECTIONS.LEFT);
 
-leftArrowEl.addEventListener('click', () => {
+leftArrowEl.addEventListener("click", () => {
   let currentIndex = images.indexOf(currentImage);
   if (currentIndex - 1 < 0) {
     currentIndex = images.length - 1;
   } else {
     currentIndex--;
   }
-  setActiveImage(images[currentIndex])
-})
+  setActiveImage(images[currentIndex], DIRECTIONS.LEFT);
+});
 
-rightArrowEl.addEventListener('click', () => {
+rightArrowEl.addEventListener("click", () => {
   let currentIndex = images.indexOf(currentImage);
   if (currentIndex + 1 === images.length) {
     currentIndex = 0;
   } else {
     currentIndex++;
   }
-  setActiveImage(images[currentIndex])
-})
+  setActiveImage(images[currentIndex], DIRECTIONS.RIGHT);
+});
